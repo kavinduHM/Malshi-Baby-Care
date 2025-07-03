514f630f2a22c535d3eaeb3a52b497d34956ed44
Kavindu N <kavindunethsara445@gmail.com>
2025-07-03 18:05:46 +0530

    Login Page Fixes and Header Layouting

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Malshi Baby Care POS</title>
+    <link rel="stylesheet" href="../Malshi Baby Care/Assets/Css/header.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="header-container">
+    <nav>
+        <ul>
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="inventory.php">Inventory</a></li>
+            <li><a href="sales-reports.php">Sales Reports</a></li>
+            <li><a href="settings.php">Settings</a></li>
+        </ul>
+    </nav>
+</div>
+
+    
+</body>
+</html>
